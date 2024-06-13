4eedb4b5f0b7421b2d1ee1099814ca75951752c7
czc <2940996969@qq.com>
2024-06-13 11:30:36 +0800

    测试

--- tanke.py
+++ tanke.py
@@ -1,5 +1,31 @@
-import pygame,sys
+import pygame, sys
 
-pygame.init()
-screensize =(1024,768)
-screen = pygame.display.set_mode(screensize,0,32)
+display = pygame.display
+COLOR_BLACK = pygame.Color(0, 0, 0)
+
+
+class MainGame():
+    # 游戏主窗口
+    window = None
+    SCREEN_HEIGHT = 500
+    SCREEN_WIDTH = 800
+
+    def init(self):
+        pass
+
+    # 开始游戏方法
+    def startGame(self):
+        display.init()
+        # 创建窗口加载窗口
+        MainGame.window = display.set_mode([MainGame.SCREEN_WIDTH, MainGame.SCREEN_HEIGHT])
+        # 设置一下游戏标题
+        display.set_caption("坦克大战 v1.03")
+        # 让窗口持续刷新操作
+        while True:
+            # 给窗口完成一个填充颜色
+            MainGame.window.fill(COLOR_BLACK)
+            # 窗口的刷新
+            display.update()
+
+
+MainGame().startGame()
