27aef72884df9f229e16154036ebae4df6a586b2
czc <2940996969@qq.com>
2024-06-13 15:15:53 +0800

    添加了我方坦克

--- main-tanke.py
+++ main-tanke.py
@@ -6,6 +6,28 @@ COLOR_BLACK = pygame.Color(0, 0, 0)
 COLOR_WRITE = pygame.Color(255, 255, 255)
 version = "0.01 Bate"
 
+class Tank():
+    def __init__(self, left, top):
+        self.images = {
+            'U': pygame.image.load('img/p1tankU.gif'),
+            'D': pygame.image.load('img/p1tankD.gif'),
+            'L': pygame.image.load('img/p1tankL.gif'),
+            'R': pygame.image.load('img/p1tankR.gif')
+        }
+        self.direction = 'U'
+        self.image = self.images[self.direction]
+        # 坦克所在的区域 Rect->
+        self.rect = self.image.get_rect()
+        self.rect.left = left
+        # 指定坦克初始化位置 分别距 x，y 轴的位置
+        self.rect.top = top
+
+    # 展示坦克(将坦克这个 surface 绘制到窗口中 blit())
+    def displayTank(self):
+        # 1.重新设置坦克的图片
+        self.image = self.images[self.direction]
+        # 2.将坦克加入到窗口中
+        MainGame.window.blit(self.image, self.rect)
 
 class MainGame():
     # 游戏主窗口
@@ -27,6 +49,8 @@ class MainGame():
         _display.init()
         # 创建窗口加载窗口
         MainGame.window = _display.set_mode([MainGame.SCREEN_WIDTH, MainGame.SCREEN_HEIGHT])
+        # 创建我方坦克
+        MainGame.TANK_P1 = Tank(400, 300)
         # 设置一下游戏标题
         _display.set_caption("坦克大战 "+version)
         # 让窗口持续刷新操作
@@ -34,6 +58,7 @@ class MainGame():
             # 给窗口完成一个填充颜色
             MainGame.window.fill(COLOR_BLACK)
             MainGame.window.blit(self.getTextSurface("剩余敌人:%d"%5),(5,5))
+            MainGame.TANK_P1.displayTank()
             # 窗口的刷新
             _display.update()
 
