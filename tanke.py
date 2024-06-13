5dd6fecd8b68eec84bd2331628b8c65a4117de62
czc <2940996969@qq.com>
2024-06-13 15:43:34 +0800

    添加了我方坦克

--- tanke.py
+++ tanke.py
@@ -16,6 +16,46 @@ class MainGame():
     def __init__(self):
         pass
 
+    def getEvent(self):
+        # 1.获取所有事件
+        eventList = pygame.event.get()
+        # 2.对事件进行判断处理(1、点击关闭按钮 2、按下键盘上的某个按键)
+        for event in eventList:
+            # 判断 event.type 是否 QUIT，如果是退出的话，直接调用程序结束方法
+            if event.type == pygame.QUIT:
+                self.endGame()
+            # 判断事件类型是否为按键按下，如果是，继续判断按键是哪一个按键，来进行对应的处理
+            if event.type == pygame.KEYDOWN:
+                # 具体是哪一个按键的处理
+                if event.key == pygame.K_LEFT:
+                    print("坦克向左调头，移动")
+                    # 修改坦克方向
+                    MainGame.TANK_P1.direction = 'L'
+                    MainGame.TANK_P1.stop = False
+                elif event.key == pygame.K_RIGHT:
+                    print("坦克向右调头，移动")
+                    # 修改坦克方向
+                    MainGame.TANK_P1.direction = 'R'
+                    MainGame.TANK_P1.stop = False
+                elif event.key == pygame.K_UP:
+                    print("坦克向上调头，移动")
+                    # 修改坦克方向
+                    MainGame.TANK_P1.direction = 'U'
+                    MainGame.TANK_P1.stop = False
+                elif event.key == pygame.K_DOWN:
+                    print("坦克向下掉头，移动")
+                    # 修改坦克方向
+                    MainGame.TANK_P1.direction = 'D'
+                    MainGame.TANK_P1.stop = False
+                elif event.key == pygame.K_SPACE:
+                    print("发射子弹")
+                # 结束游戏方法
+                if event.type == pygame.KEYUP:
+                    # 松开的如果是方向键，才更改移动开关状态
+                    if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                        # 修改坦克的移动状态
+                        MainGame.TANK_P1.stop = True
+
     def getTextSurface(self, text):
         _font.init()
         font = _font.SysFont("kaiti", 18)
