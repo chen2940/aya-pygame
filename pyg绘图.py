65ac04e0fa559feb5545b25d0881d33d043a98c3
chen2940 <81813862+chen2940@users.noreply.github.com>
2024-06-13 09:01:13 +0800

    Add files via upload

---
+++ pyg绘图.py
@@ -0,0 +1,11 @@
+import pygame, sys
+
+pygame.init()
+
+size = chang, kuan = 600, 400
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+# pygame.display.set_icon("")
+BLACK = 0, 0, 0
+fps = 120
+fclock = pygame.time.Clock()
